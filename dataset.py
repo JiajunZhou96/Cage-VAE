27e5260ec9c7973bd6b19ddcbae9260683e987c9
Jiajun Zhou <91960648+JiajunZhou96@users.noreply.github.com>
2023-10-12 16:37:18 +0100

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -133,3 +133,100 @@ def data_prep(dataset, undataset):
     test_dataloader = DataLoader(all_test_data, batch_size = len(all_test_data), shuffle = True, worker_init_fn = seed_worker, generator = G)
     
     return train_dataloader, test_dataloader
+
+import selfies as sf
+
+def data_prep_selfie(dataset, undataset):
+    
+    dataset['bb1_skeleton'] = dataset.apply(lambda x:eutils.obtain_skeleton(x['bb1_smile'], bb_type = 3), axis=1)
+    dataset['bb2_skeleton'] = dataset.apply(lambda x:eutils.obtain_skeleton(x['bb2_smile'], bb_type = 2), axis=1)
+    dataset['collapsed'].fillna(value = 1, inplace = True)
+    
+    dataset['bb2_selfie_sk'] = eutils.to_selfies(dataset['bb2_skeleton'])
+    undataset['bb2_selfie_sk'] = eutils.to_selfies(undataset['bb2_skeleton'])
+    
+    max_len = 57
+    
+    #selfie_char = list(sf.get_alphabet_from_selfies(undataset['bb2_selfie_sk'].append( dataset['bb2_selfie_sk'])))
+    #special_char = ['[nop]','[sos]','[eos]']
+    #selfie_char_list = special_char + selfie_char
+    selfie_char_list = utils.load_vocab("./vocab/selfie_vocab.npy")
+    
+    data_prep_selfie.selfie_to_index = dict((c,i) for i,c in enumerate(selfie_char_list))
+    data_prep_selfie.index_to_selfie = dict((i,c) for i,c in enumerate(selfie_char_list))
+
+    vocab_length = eutils.selfie_vocab_len(selfie_char_list)
+
+    selfies_sos = np.array([eutils.add_sos(smile, mode = 'selfie') for smile in dataset['bb2_selfie_sk'] ])
+    selfies_sos_eos = np.array([eutils.add_eos(smile, mode = 'selfie') for smile in selfies_sos])
+
+    un_selfies_sos = np.array([eutils.add_sos(smile, mode = 'selfie') for smile in undataset['bb2_selfie_sk'] ])
+    un_selfies_sos_eos = np.array([eutils.add_eos(smile, mode = 'selfie') for smile in un_selfies_sos])
+
+    length_list = [len(smile) for smile in selfies_sos_eos]
+    selfie2id_list = eutils.selfies_to_idx(selfies_sos_eos, max_len, data_prep_selfie.selfie_to_index)
+
+    un_length_list = [len(smile) for smile in un_selfies_sos_eos]
+    un_selfie2id_list = eutils.selfies_to_idx(un_selfies_sos_eos, max_len, data_prep_selfie.selfie_to_index)
+
+    data_prep_selfie.ordinalenc = OrdinalEncoder()
+    ordinal_treat = pd.concat([dataset['bb1_skeleton'], dataset['reaction']], axis = 1)
+
+    data_prep_selfie.ordinalenc.fit(ordinal_treat)
+    features_list = data_prep_selfie.ordinalenc.transform(ordinal_treat)
+
+    un_ordinal_treat = pd.concat([undataset['bb1_skeleton'], undataset['reaction']], axis = 1)
+    un_features_list = data_prep_selfie.ordinalenc.transform(un_ordinal_treat)
+
+    collapse_label = dataset['collapsed'] # shape persistency 输入！！
+
+    undataset['collapsed'] = -1  # 设置为 -1
+    uncollapse_label = undataset['collapsed'] # shape persistency 输入！！
+
+    dataset['indicator'] = 1
+    undataset['indicator'] = 0
+
+    data_prep_selfie.supervised_input = np.hstack((np.array(selfie2id_list), 
+                                    np.array(length_list)[:, np.newaxis], 
+                                    features_list, 
+                                    np.array(collapse_label)[:, np.newaxis],  
+                                    dataset['indicator'].to_numpy()[:, np.newaxis])) 
+
+    data_prep_selfie.unsupervised_input = np.hstack((np.array(un_selfie2id_list), 
+                                    np.array(un_length_list)[:, np.newaxis], 
+                                    un_features_list, 
+                                    np.array(uncollapse_label)[:, np.newaxis],  
+                                    undataset['indicator'].to_numpy()[:, np.newaxis]))
+
+    supervised_train, supervised_test = train_test_split(data_prep_selfie.supervised_input, test_size = 0.1, random_state = seed)
+    unsupervised_train, unsupervised_test = train_test_split(data_prep_selfie.unsupervised_input, test_size = 0.002, random_state = seed)
+
+    data_prep_selfie.train_mixed_data = np.vstack((supervised_train, unsupervised_train))
+    data_prep_selfie.test_mixed_data = np.vstack((supervised_test, unsupervised_test))
+
+    np.random.shuffle(data_prep_selfie.train_mixed_data)
+    np.random.shuffle(data_prep_selfie.test_mixed_data)
+
+    train_data = torch.from_numpy(data_prep_selfie.train_mixed_data[:,:max_len]).type(torch.LongTensor)
+    test_data = torch.from_numpy(data_prep_selfie.test_mixed_data[:,:max_len]).type(torch.LongTensor)
+
+    train_len = torch.from_numpy(data_prep_selfie.train_mixed_data[:,max_len]).type(torch.LongTensor)
+    test_len = torch.from_numpy(data_prep_selfie.test_mixed_data[:,max_len]).type(torch.LongTensor)
+
+    train_features = torch.from_numpy(data_prep_selfie.train_mixed_data[:,max_len + 1:max_len + 3]).type(torch.LongTensor)
+    test_features = torch.from_numpy(data_prep_selfie.test_mixed_data[:,max_len + 1:max_len + 3]).type(torch.LongTensor)
+
+    train_target = torch.from_numpy(data_prep_selfie.train_mixed_data[:,max_len + 3]).type(torch.LongTensor)
+    test_target = torch.from_numpy(data_prep_selfie.test_mixed_data[:,max_len + 3]).type(torch.LongTensor)
+
+    train_indicator = torch.from_numpy(data_prep_selfie.train_mixed_data[:,max_len + 4]).type(torch.LongTensor)
+    test_indicator = torch.from_numpy(data_prep_selfie.test_mixed_data[:,max_len + 4]).type(torch.LongTensor)
+
+    all_train_data = TensorDataset(train_data, train_len, train_features, train_target, train_indicator)
+    all_test_data = TensorDataset(test_data, test_len, test_features, test_target, test_indicator)
+
+    train_dataloader = DataLoader(all_train_data, batch_size = 128, shuffle = True, worker_init_fn = seed_worker, generator = G)
+    test_dataloader = DataLoader(all_test_data, batch_size = len(all_test_data), shuffle = True, worker_init_fn = seed_worker, generator = G)
+    
+    return train_dataloader, test_dataloader
+    
